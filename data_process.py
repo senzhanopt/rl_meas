c6a468dde9711c66779c59cfe05006d79d4a8eac
Sen Zhan <s.zhan@tue.nl>
2024-10-15 17:09:00 +0200

    include new case

--- data_process.py
+++ data_process.py
@@ -1,1 +1,43 @@
 import simbench as sb
+import pandapower as pp
+import time
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+start, end = 240,247
+net = sb.get_simbench_net('1-LV-rural2--2-no_sw')
+net.ext_grid.vm_pu = 1.0
+net.sgen = pd.read_excel('sgen.xlsx', index_col = 0).iloc[0:48,:]
+pp.plotting.to_html(net, 'grid.html')
+n_bus = len(net.bus)
+
+
+prof = sb.get_absolute_values(net, 1)
+load_p = prof[('load', 'p_mw')].iloc[start*96:end*96,:].to_numpy()
+load_q = prof[('load', 'q_mvar')].iloc[start*96:end*96,:].to_numpy()
+sgen_p = sb.get_absolute_profiles_from_relative_profiles(net, 'sgen', 'sn_mva').iloc[start*96:end*96,:].to_numpy()
+
+
+vm = np.ones(((end-start)*96, n_bus))
+loading_trafo = np.zeros((end-start)*96)
+
+for t in range(96*(end-start)):
+    net.load.p_mw = load_p[t,:]
+    net.load.q_mvar = load_q[t,:]
+    net.sgen.p_mw = sgen_p[t,:]
+    net.sgen.q_mvar = 0
+    net.storage.p_mw = 0
+    net.storage.q_mvar = 0
+    pp.runpp(net)
+    vm[t,:] = net.res_bus.vm_pu.to_numpy()
+    loading_trafo[t] = net.res_trafo.loading_percent[0]
+    
+    
+for b in [55,66,59,70]:
+    plt.plot(vm[:,b], label = f'bus {b}')
+plt.legend()
+plt.show()
+    
+plt.plot(loading_trafo)
+plt.show()
