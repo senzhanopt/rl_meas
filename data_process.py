b62b2110fb6a60fbb7bb5f600deb0b33cbc736b1
Sen Zhan <s.zhan@tue.nl>
2024-10-17 16:28:20 +0200

    add soc and remove slack bus from voltage profiles

--- data_process.py
+++ data_process.py
@@ -12,6 +12,7 @@ n_itr = 15 # one-minute control resolution
 n_pv = 48 # 50% nodes have a pv
 n_storage = 19 # 20% nodes have a battery
 list_bus_visual = [55,66,59,70]
+soc_min, soc_max, soc_init = 0.2, 0.8, 0.5
 
 # read pp net object from simbench
 net = sb.get_simbench_net('1-LV-rural2--2-no_sw')
@@ -19,7 +20,7 @@ net.ext_grid.vm_pu = 1.0
 net.trafo.tap_pos = 0
 net.sgen = pd.read_excel('sgen.xlsx', index_col = 0).iloc[0:n_pv,:]
 net.storage = pd.read_excel('storage.xlsx', index_col = 0).iloc[0:n_storage,:]
-pp.plotting.to_html(net, 'grid.html')
+#pp.plotting.to_html(net, 'grid.html')
 n_bus = len(net.bus) # 97 bus
 
 # read load profiles
@@ -32,7 +33,7 @@ sgen_p = sb.get_absolute_profiles_from_relative_profiles(net, 'sgen', 'sn_mva').
 storage_p = sb.get_absolute_profiles_from_relative_profiles(net, 'storage', 'sn_mva').iloc[start*96:end*96,:].to_numpy()
 
 # power flow with default storage profiles
-vm = np.ones(((end-start)*96, n_bus))
+vm = np.ones(((end-start)*96, n_bus-1))
 loading_trafo = np.zeros((end-start)*96)
 for t in range(96*(end-start)):
     net.load.p_mw = load_p[t,:]
@@ -42,12 +43,12 @@ for t in range(96*(end-start)):
     net.storage.p_mw = storage_p[t,:]
     net.storage.q_mvar = 0
     pp.runpp(net)
-    vm[t,:] = net.res_bus.vm_pu.to_numpy()
+    vm[t,:] = net.res_bus.vm_pu.to_numpy()[1:]
     loading_trafo[t] = net.res_trafo.loading_percent[0]
     
     
 for b in list_bus_visual:
-    plt.plot(vm[:,b], label = f'bus {b}')
+    plt.plot(vm[:,b-1], label = f'bus {b}')
 plt.legend()
 plt.show()
     
