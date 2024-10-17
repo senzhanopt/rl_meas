5b5e3be5b17a701103d3c2b6df6b471d3dae09fe
Sen Zhan <s.zhan@tue.nl>
2024-10-17 19:41:59 +0200

    add pv capacity and battery specs

--- data_process.py
+++ data_process.py
@@ -13,6 +13,7 @@ n_pv = 48 # 50% nodes have a pv
 n_storage = 19 # 20% nodes have a battery
 list_bus_visual = [55,66,59,70]
 soc_min, soc_max, soc_init = 0.2, 0.8, 0.5
+eta_ch, eta_dis = 0.95, 0.95
 
 # read pp net object from simbench
 net = sb.get_simbench_net('1-LV-rural2--2-no_sw')
@@ -22,6 +23,9 @@ net.sgen = pd.read_excel('sgen.xlsx', index_col = 0).iloc[0:n_pv,:]
 net.storage = pd.read_excel('storage.xlsx', index_col = 0).iloc[0:n_storage,:]
 #pp.plotting.to_html(net, 'grid.html')
 n_bus = len(net.bus) # 97 bus
+S_pv = net.sgen.sn_mva.to_numpy() * 1E3
+S_storage = net.storage.sn_mva.to_numpy() * 1E3
+E_storage = net.storage.max_e_mwh.to_numpy() * 1E3
 
 # read load profiles
 prof = sb.get_absolute_values(net, 1)
