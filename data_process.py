e150836d651a123f8a132fbf13bcff0524a3e192
Sen Zhan <s.zhan@tue.nl>
2024-10-17 17:05:40 +0200

    add network sensitivity

--- data_process.py
+++ data_process.py
@@ -39,13 +39,12 @@ for t in range(96*(end-start)):
     net.load.p_mw = load_p[t,:]
     net.load.q_mvar = load_q[t,:]
     net.sgen.p_mw = sgen_p[t,:]
-    net.sgen.q_mvar = 0
+    net.sgen.q_mvar = 0.0
     net.storage.p_mw = storage_p[t,:]
-    net.storage.q_mvar = 0
+    net.storage.q_mvar = 0.0
     pp.runpp(net)
     vm[t,:] = net.res_bus.vm_pu.to_numpy()[1:]
     loading_trafo[t] = net.res_trafo.loading_percent[0]
-    
     
 for b in list_bus_visual:
     plt.plot(vm[:,b-1], label = f'bus {b}')
@@ -57,5 +56,48 @@ plt.legend()
 plt.show()
 
 
+#%% network sensitivity
+mat_R_storage = np.zeros((n_bus-1, n_storage))
+mat_X_storage = np.zeros((n_bus-1, n_storage))
+mat_R_pv = np.zeros((n_bus-1, n_pv))
+mat_X_pv = np.zeros((n_bus-1, n_pv))
+
+# base case
+net.load.p_mw = 0.0
+net.load.q_mvar = 0.0
+net.sgen.p_mw = 0.0
+net.sgen.q_mvar = 0.0
+net.storage.p_mw = 0.0
+net.storage.q_mvar = 0.0
+pp.runpp(net)
+v_base = net.res_bus.vm_pu.to_numpy()[1:]
+
+for b in range(n_storage):
+    net.storage.loc[b, 'p_mw'] = 1E-3
+    pp.runpp(net)
+    v_current = net.res_bus.vm_pu.to_numpy()[1:]
+    mat_R_storage[:,b] = v_current - v_base
+    net.storage.loc[b, 'p_mw'] = 0.0 # recover the base case
+
+for b in range(n_storage):
+    net.storage.loc[b, 'q_mvar'] = 1E-3
+    pp.runpp(net)
+    v_current = net.res_bus.vm_pu.to_numpy()[1:]
+    mat_X_storage[:,b] = v_current - v_base
+    net.storage.loc[b, 'q_mvar'] = 0.0 # recover the base case
+
+for b in range(n_pv):
+    net.sgen.loc[b, 'p_mw'] = 1E-3
+    pp.runpp(net)
+    v_current = net.res_bus.vm_pu.to_numpy()[1:]
+    mat_R_pv[:,b] = v_current - v_base
+    net.sgen.loc[b, 'p_mw'] = 0.0 # recover the base case
+
+for b in range(n_pv):
+    net.sgen.loc[b, 'q_mvar'] = 1E-3
+    pp.runpp(net)
+    v_current = net.res_bus.vm_pu.to_numpy()[1:]
+    mat_X_pv[:,b] = v_current - v_base
+    net.sgen.loc[b, 'q_mvar'] = 0.0 # recover the base case
 
 
