8f56b257987ea852532e12a8b6a8b006c5e2196c
Sen Zhan <senzhanopt@gmail.com>
2024-10-17 23:12:42 +0200

    use kw for profiles AND split network sensivitity calculation/powe

--- data_process.py
+++ data_process.py
@@ -30,79 +30,12 @@ E_storage = net.storage.max_e_mwh.to_numpy() * 1E3
 
 # read load profiles
 prof = sb.get_absolute_values(net, 1)
-load_p = prof[('load', 'p_mw')].iloc[start*96:end*96,:].to_numpy()
-load_q = prof[('load', 'q_mvar')].iloc[start*96:end*96,:].to_numpy()
+load_p = prof[('load', 'p_mw')].iloc[start*96:end*96,:].to_numpy() * 1E3
+load_q = prof[('load', 'q_mvar')].iloc[start*96:end*96,:].to_numpy() * 1E3
 
 # match sgen and storage profiles
-sgen_p = sb.get_absolute_profiles_from_relative_profiles(net, 'sgen', 'sn_mva').iloc[start*96:end*96,:].to_numpy()
-storage_p = sb.get_absolute_profiles_from_relative_profiles(net, 'storage', 'sn_mva').iloc[start*96:end*96,:].to_numpy()
+sgen_p = sb.get_absolute_profiles_from_relative_profiles(net, 'sgen', 'sn_mva').iloc[start*96:end*96,:].to_numpy() * 1E3
+storage_p = sb.get_absolute_profiles_from_relative_profiles(net, 'storage', 'sn_mva').iloc[start*96:end*96,:].to_numpy() * 1E3
 
-# power flow with default storage profiles
-vm = np.ones(((end-start)*96, n_bus-1))
-loading_trafo = np.zeros((end-start)*96)
-for t in range(96*(end-start)):
-    net.load.p_mw = load_p[t,:]
-    net.load.q_mvar = load_q[t,:]
-    net.sgen.p_mw = sgen_p[t,:]
-    net.sgen.q_mvar = 0.0
-    net.storage.p_mw = storage_p[t,:]
-    net.storage.q_mvar = 0.0
-    pp.runpp(net)
-    vm[t,:] = net.res_bus.vm_pu.to_numpy()[1:]
-    loading_trafo[t] = net.res_trafo.loading_percent[0]
-    
-for b in list_bus_visual:
-    plt.plot(vm[:,b-1], label = f'bus {b}')
-plt.legend()
-plt.show()
-    
-plt.plot(loading_trafo, label = "MV/LV transformer")
-plt.legend()
-plt.show()
-
-
-#%% network sensitivity
-mat_R_storage = np.zeros((n_bus-1, n_storage))
-mat_X_storage = np.zeros((n_bus-1, n_storage))
-mat_R_pv = np.zeros((n_bus-1, n_pv))
-mat_X_pv = np.zeros((n_bus-1, n_pv))
-
-# base case
-net.load.p_mw = 0.0
-net.load.q_mvar = 0.0
-net.sgen.p_mw = 0.0
-net.sgen.q_mvar = 0.0
-net.storage.p_mw = 0.0
-net.storage.q_mvar = 0.0
-pp.runpp(net)
-v_base = net.res_bus.vm_pu.to_numpy()[1:]
-
-for b in range(n_storage):
-    net.storage.loc[b, 'p_mw'] = 1E-3
-    pp.runpp(net)
-    v_current = net.res_bus.vm_pu.to_numpy()[1:]
-    mat_R_storage[:,b] = v_current - v_base
-    net.storage.loc[b, 'p_mw'] = 0.0 # recover the base case
-
-for b in range(n_storage):
-    net.storage.loc[b, 'q_mvar'] = 1E-3
-    pp.runpp(net)
-    v_current = net.res_bus.vm_pu.to_numpy()[1:]
-    mat_X_storage[:,b] = v_current - v_base
-    net.storage.loc[b, 'q_mvar'] = 0.0 # recover the base case
-
-for b in range(n_pv):
-    net.sgen.loc[b, 'p_mw'] = 1E-3
-    pp.runpp(net)
-    v_current = net.res_bus.vm_pu.to_numpy()[1:]
-    mat_R_pv[:,b] = v_current - v_base
-    net.sgen.loc[b, 'p_mw'] = 0.0 # recover the base case
-
-for b in range(n_pv):
-    net.sgen.loc[b, 'q_mvar'] = 1E-3
-    pp.runpp(net)
-    v_current = net.res_bus.vm_pu.to_numpy()[1:]
-    mat_X_pv[:,b] = v_current - v_base
-    net.sgen.loc[b, 'q_mvar'] = 0.0 # recover the base case
 
 
---
+++ get_profiles_sensitivities.py
@@ -0,0 +1,75 @@
+from data_process import *
+
+# power flow with default storage profiles
+vm = np.ones(((end-start)*96, n_bus-1))
+loading_trafo = np.zeros((end-start)*96)
+for t in range(96*(end-start)):
+    net.load.p_mw = load_p[t,:] * 1E-3
+    net.load.q_mvar = load_q[t,:] * 1E-3
+    net.sgen.p_mw = sgen_p[t,:] * 1E-3
+    net.sgen.q_mvar = 0.0
+    net.storage.p_mw = storage_p[t,:] * 1E-3
+    net.storage.q_mvar = 0.0
+    pp.runpp(net)
+    vm[t,:] = net.res_bus.vm_pu.to_numpy()[1:]
+    loading_trafo[t] = net.res_trafo.loading_percent[0]
+    
+for b in list_bus_visual:
+    plt.plot(vm[:,b-1], label = f'bus {b}')
+plt.legend()
+plt.show()
+    
+plt.plot(loading_trafo, label = "MV/LV transformer")
+plt.legend()
+plt.show()
+
+
+#%% network sensitivity
+mat_R_storage = np.zeros((n_bus-1, n_storage))
+mat_X_storage = np.zeros((n_bus-1, n_storage))
+mat_R_pv = np.zeros((n_bus-1, n_pv))
+mat_X_pv = np.zeros((n_bus-1, n_pv))
+
+# base case
+net.load.p_mw = 0.0
+net.load.q_mvar = 0.0
+net.sgen.p_mw = 0.0
+net.sgen.q_mvar = 0.0
+net.storage.p_mw = 0.0
+net.storage.q_mvar = 0.0
+pp.runpp(net)
+v_base = net.res_bus.vm_pu.to_numpy()[1:]
+
+for b in range(n_storage):
+    net.storage.loc[b, 'p_mw'] = 1E-3
+    pp.runpp(net)
+    v_current = net.res_bus.vm_pu.to_numpy()[1:]
+    mat_R_storage[:,b] = v_current - v_base
+    net.storage.loc[b, 'p_mw'] = 0.0 # recover the base case
+
+for b in range(n_storage):
+    net.storage.loc[b, 'q_mvar'] = 1E-3
+    pp.runpp(net)
+    v_current = net.res_bus.vm_pu.to_numpy()[1:]
+    mat_X_storage[:,b] = v_current - v_base
+    net.storage.loc[b, 'q_mvar'] = 0.0 # recover the base case
+
+for b in range(n_pv):
+    net.sgen.loc[b, 'p_mw'] = 1E-3
+    pp.runpp(net)
+    v_current = net.res_bus.vm_pu.to_numpy()[1:]
+    mat_R_pv[:,b] = v_current - v_base
+    net.sgen.loc[b, 'p_mw'] = 0.0 # recover the base case
+
+for b in range(n_pv):
+    net.sgen.loc[b, 'q_mvar'] = 1E-3
+    pp.runpp(net)
+    v_current = net.res_bus.vm_pu.to_numpy()[1:]
+    mat_X_pv[:,b] = v_current - v_base
+    net.sgen.loc[b, 'q_mvar'] = 0.0 # recover the base case
+
+pd.DataFrame(mat_R_storage).to_csv('mat_R_storage.csv')
+pd.DataFrame(mat_X_storage).to_csv('mat_X_storage.csv')
+pd.DataFrame(mat_R_pv).to_csv('mat_R_pv.csv')
+pd.DataFrame(mat_X_pv).to_csv('mat_X_pv.csv')
+
