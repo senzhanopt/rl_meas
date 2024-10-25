4984dc6d6770674f04bfc8024e00cf1e38367107
Sen Zhan <s.zhan@tue.nl>
2024-10-25 16:13:35 +0200

    ofo w/ loading constraint

--- data_process.py
+++ data_process.py
@@ -15,6 +15,8 @@ n_storage = 19 # 20% nodes have a battery
 list_bus_visual = [55,66,59,70]
 soc_min, soc_max, soc_init = 0.2, 0.8, 0.5
 eta_ch, eta_dis = 0.95, 0.95
+v_upp = 1.05
+v_low = 0.95
 
 # read pp net object from simbench
 net = sb.get_simbench_net('1-LV-rural2--2-no_sw')
@@ -27,6 +29,7 @@ n_bus = len(net.bus) # 97 bus
 S_pv = net.sgen.sn_mva.to_numpy() * 1E3
 S_storage = net.storage.sn_mva.to_numpy() * 1E3
 E_storage = net.storage.max_e_mwh.to_numpy() * 1E3
+S_trafo = net.trafo.sn_mva[0] * 1E3
 
 # read load profiles
 prof = sb.get_absolute_values(net, 1)
--- ofo.py
+++ ofo.py
@@ -1,17 +1,19 @@
 from data_process import *
 import numpy as np
 from tqdm import tqdm
+import copy
 
 ## parameters
-beta = 1E6
-alpha = 0.5
-v_upp = 1.05
-v_low = 0.95
-start_ofo, end_ofo = 1, 2
-n_timesteps = (end_ofo-start_ofo)*96
-load_p = load_p[start_ofo*96:end_ofo*96,:]
-load_q = load_q[start_ofo*96:end_ofo*96,:]
-sgen_p = sgen_p[start_ofo*96:end_ofo*96,:]
+beta = 0.0
+rho = 5E0
+alpha = 0.1
+xi = 0.1
+n_itr = 100
+start_ofo, end_ofo = 48+96,49+96
+n_timesteps = end_ofo-start_ofo
+load_p = load_p[start_ofo:end_ofo,:]
+load_q = load_q[start_ofo:end_ofo,:]
+sgen_p = sgen_p[start_ofo:end_ofo,:]
 
 mat_R_storage = pd.read_csv('mat_R_storage.csv', index_col = 0).to_numpy()
 mat_X_storage = pd.read_csv('mat_X_storage.csv', index_col = 0).to_numpy()
@@ -62,6 +64,7 @@ def projection_pv(P, S, p, q):
         elif (p <= 0) and (q <= -S):
             return np.array([0, -S])
         else:
+            print(f"P, S, p, q values are {P}, {S}, {p}, {q}.")
             raise Exception("Projection fails!")
             
 def projection_storage(Pmax, Pmin, S, p, q): # charging power is positive
@@ -92,18 +95,19 @@ mat_soc_storage = np.zeros((n_timesteps*n_itr+1, n_storage))
 mat_p_pv = np.zeros((n_timesteps*n_itr, n_pv))
 mat_q_pv = np.zeros((n_timesteps*n_itr, n_pv))
 mat_v = np.ones((n_timesteps*n_itr, n_bus-1))
-mat_loading = np.zeros((n_timesteps*n_itr, 1))
+mat_loading = np.zeros(n_timesteps*n_itr)
 mat_lambdas = np.zeros((n_timesteps*n_itr, n_bus-1))
 mat_gamma = np.zeros((n_timesteps*n_itr, n_bus-1))
-mat_pi = np.zeros((n_timesteps*n_itr, 1))
+mat_pi = np.zeros(n_timesteps*n_itr)
 
 # initialize
-p_pv = sgen_p[0,:] 
+p_pv = copy.deepcopy(sgen_p[0,:]) 
 q_pv = np.zeros(n_pv) 
 p_storage = np.zeros(n_storage) 
 q_storage = np.zeros(n_storage) 
 lambdas = np.zeros(n_bus-1)
 gamma = np.zeros(n_bus-1)
+pi = 0.0
 soc = soc_init * np.ones(n_storage)
 mat_soc_storage[0,:] = soc
 
@@ -126,13 +130,19 @@ for itr in tqdm(range(n_timesteps * n_itr)):
     # power flow
     net.load.p_mw = load_p_current * 1E-3
     net.load.q_mvar = load_q_current * 1E-3
-    net.sgen.p_mw = sgen_p_current * 1E-3
-    net.sgen.q_mvar = 0
+    net.sgen.p_mw = p_pv * 1E-3
+    net.sgen.q_mvar = q_pv * 1E-3
     net.storage.p_mw = p_storage * 1E-3
     net.storage.q_mvar = q_storage * 1E-3
     pp.runpp(net)
     v = net.res_bus.vm_pu.to_numpy()[1:]
+    loading = net.res_trafo.loading_percent[0]
+    P_trafo = net.res_trafo.p_hv_mw[0] * 1E3
+    Q_trafo = net.res_trafo.q_hv_mvar[0] * 1E3
+    pf_trafo = P_trafo / np.sqrt(P_trafo**2+Q_trafo**2)
+    rpf_trafo = Q_trafo / np.sqrt(P_trafo**2+Q_trafo**2)
     mat_v[itr, :] = v
+    mat_loading[itr] = loading
 
     
     # dual gradient ascent steps
@@ -140,10 +150,19 @@ for itr in tqdm(range(n_timesteps * n_itr)):
     gamma += beta * (v_low - v)
     lambdas *= (lambdas >= 0)
     gamma *= (gamma >= 0)
+    pi += rho * (loading - 1.0)
+    pi *= (pi >= 0)
     
     # primal gradient descent
-    grad_p_storage = p_storage + mat_R_storage.T @ (lambdas - gamma)
-    grad_q_storage = 0.1*p_storage + mat_X_storage.T @ (lambdas - gamma)
+    grad_p_pv = p_pv - sgen_p_current + mat_R_pv.T @ (lambdas - gamma) - pi*pf_trafo/S_trafo*np.ones(n_pv)
+    grad_q_pv = xi * q_pv + mat_X_pv.T @ (lambdas - gamma) - pi*rpf_trafo/S_trafo*np.ones(n_pv)
+    p_pv -= alpha * grad_p_pv
+    q_pv -= alpha * grad_q_pv
+    for i in range(n_pv):
+        p_pv[i], q_pv[i] = projection_pv(sgen_p_current[i], S_pv[i], p_pv[i], q_pv[i])
+    
+    grad_p_storage = p_storage + mat_R_storage.T @ (lambdas - gamma) + pi*pf_trafo/S_trafo*np.ones(n_storage)
+    grad_q_storage = xi*q_storage + mat_X_storage.T @ (lambdas - gamma) + pi*rpf_trafo/S_trafo*np.ones(n_storage)
     p_storage -= alpha * grad_p_storage
     q_storage -= alpha * grad_q_storage
     Pmax = (soc_max - soc)*E_storage/itr_length/eta_ch
@@ -156,10 +175,13 @@ for itr in tqdm(range(n_timesteps * n_itr)):
 
 # visualization
 for b in list_bus_visual:
-    plt.plot(mat_v[600:840,b-1], label = f'bus {b}')
+    plt.plot(mat_v[:,b-1], label = f'bus {b}')
 plt.legend()
 plt.show()    
 
+plt.plot(mat_loading, label = "trafo")
+plt.legend()
+plt.show()
 
 for i in range(4):
     plt.plot(mat_p_storage[:,i], label = f'storage {b}')
