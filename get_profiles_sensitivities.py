848bbf754328e2d6aa637724197dd65f56b8089c
Sen Zhan <s.zhan@tue.nl>
2024-12-09 12:24:21 +0100

    change initialization

--- get_profiles_sensitivities.py
+++ get_profiles_sensitivities.py
@@ -14,7 +14,7 @@ for t in range(96*(end-start)):
     vm[t,:] = net.res_bus.vm_pu.to_numpy()[1:]
     loading_trafo[t] = net.res_trafo.loading_percent[0]
 
-"""
+
 for b in list_bus_visual:
     plt.plot(vm[:,b-1], label = f'bus {b}')
 plt.legend()
@@ -23,7 +23,7 @@ plt.show()
 plt.plot(loading_trafo, label = "MV/LV transformer")
 plt.legend()
 plt.show()
-"""
+
 
 #%% network sensitivity
 mat_R_storage = np.zeros((n_bus-1, n_storage))
@@ -34,12 +34,13 @@ mat_X_pv = np.zeros((n_bus-1, n_pv))
 # base case
 net.load.p_mw = 0.0
 net.load.q_mvar = 0.0
-net.sgen.p_mw = 0.0
+net.sgen.p_mw = S_pv * 0.9E-3
 net.sgen.q_mvar = 0.0
 net.storage.p_mw = 0.0
 net.storage.q_mvar = 0.0
 pp.runpp(net)
 v_base = net.res_bus.vm_pu.to_numpy()[1:]
+
 
 for b in range(n_storage):
     net.storage.loc[b, 'p_mw'] = 1E-3
@@ -56,24 +57,22 @@ for b in range(n_storage):
     net.storage.loc[b, 'q_mvar'] = 0.0 # recover the base case
 
 for b in range(n_pv):
-    net.sgen.loc[b, 'p_mw'] = 1E-3
+    net.sgen.loc[b, 'p_mw'] += 1E-3
     pp.runpp(net)
     v_current = net.res_bus.vm_pu.to_numpy()[1:]
     mat_R_pv[:,b] = v_current - v_base
-    net.sgen.loc[b, 'p_mw'] = 0.0 # recover the base case
+    net.sgen.loc[b, 'p_mw'] -= 1E-3 # recover the base case
 
 for b in range(n_pv):
-    net.sgen.loc[b, 'q_mvar'] = 1E-3
+    net.sgen.loc[b, 'q_mvar'] += 1E-3
     pp.runpp(net)
     v_current = net.res_bus.vm_pu.to_numpy()[1:]
     mat_X_pv[:,b] = v_current - v_base
-    net.sgen.loc[b, 'q_mvar'] = 0.0 # recover the base case
+    net.sgen.loc[b, 'q_mvar'] -= 1E-3 # recover the base case
 
 
-
-"""
 pd.DataFrame(mat_R_storage).to_csv('mat_R_storage.csv')
 pd.DataFrame(mat_X_storage).to_csv('mat_X_storage.csv')
 pd.DataFrame(mat_R_pv).to_csv('mat_R_pv.csv')
 pd.DataFrame(mat_X_pv).to_csv('mat_X_pv.csv')
-"""
+
